ee474b560e3475c8c49c778ee2498a02692e4fe7
Daniel <86088141+dannyboi1313@users.noreply.github.com>
2023-10-21 04:53:01 -0600

    Update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -155,8 +155,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="Daniel Blanchard - Portfolio" content="Daniel Blanchard Portfolio" />
+        <title>Daniel Blanchard - Portfolio</title>
+        <meta name="description" content="Daniel Blanchard Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
